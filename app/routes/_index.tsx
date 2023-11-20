406c2cea0d83de9c96c3df94b585e9508f93574e
Tapas Adhikary <tapas.adhikary@gmail.com>
2023-11-20 11:18:43 +0530

    Pushed the initial files with tailwind config

---
+++ app/routes/_index.tsx
@@ -0,0 +1,16 @@
+import type { MetaFunction } from "@remix-run/node";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "New Remix App" },
+    { name: "description", content: "Welcome to Remix!" },
+  ];
+};
+
+export default function Index() {
+  return (
+    <div>
+      <h1  className="text-3xl font-bold underline">Welcome to RemixNode</h1>
+    </div>
+  );
+}
